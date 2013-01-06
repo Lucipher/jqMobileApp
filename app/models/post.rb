9c3c6c1393309de24561dbdfc17d787e03db9715
mgadiraju <manohar.gadiraju@live.com>
2013-01-06 16:06:03 -0500

    changes to MimeTypes file, post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,12 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class Post < ActiveRecord::Base
   rolify
   belongs_to :user
-  
+  attr_accessible :title, :body, :user
+  validates :title, :body, :presence => true 
 end
